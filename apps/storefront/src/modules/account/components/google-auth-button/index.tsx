c40c7ad495a648796b4a25c763b5b02d7b84776c
mikeskafld <mike@skafldstudio.com>
2026-05-11 14:57:29 -0700

    fix(storefront): use stable google oauth callback uri

--- apps/storefront/src/modules/account/components/google-auth-button/index.tsx
+++ apps/storefront/src/modules/account/components/google-auth-button/index.tsx
@@ -24,10 +24,6 @@ const GoogleAuthButton = ({
         window.location.origin
       )
 
-      if (redirectTo?.startsWith("/")) {
-        callbackUrl.searchParams.set("redirect", redirectTo)
-      }
-
       const result = await sdk.auth.login("customer", "google", {
         callback_url: callbackUrl.toString(),
       })
